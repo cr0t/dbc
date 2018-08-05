01c997035a2b84a233cacac34b6040056bf2f09d
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2018-08-05 11:58:46 +0400

    Persistence to disk

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -1,9 +1,14 @@
 describe 'database' do
   EXECUTABLE_BIN = './a.out'
+  TEST_DB = 'test.db'
+
+  before do
+    `rm -rf #{TEST_DB}`
+  end
 
   def run_script(commands)
     raw_output = nil
-    IO.popen(EXECUTABLE_BIN, 'r+') do |pipe|
+    IO.popen("#{EXECUTABLE_BIN} #{TEST_DB}", 'r+') do |pipe|
       commands.each do |command|
         pipe.puts command
       end
@@ -82,6 +87,26 @@ describe 'database' do
     expect(result).to match_array([
       'db > ID must be positive.',
       'db > Executed.',
+      'db > ',
+    ])
+  end
+
+  it 'keeps data after closing connection' do
+    result1 = run_script([
+      'insert 1 user1 person1@example.com',
+      '.exit'
+    ])
+    expect(result1).to match_array([
+      'db > Executed.',
+      'db > ',
+    ])
+    result2 = run_script([
+      'select',
+      '.exit'
+    ])
+    expect(result2).to match_array([
+      'db > (1, user1, person1@example.com)',
+      'Executed.',
       'db > ',
     ])
   end
